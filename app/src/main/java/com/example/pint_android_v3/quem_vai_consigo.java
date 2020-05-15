52acbcb9d843416697c1463ed35465411fa42f93
Joao Estima <joao.pedro.estima@hotmail.com>
2020-05-15 19:37:55 +0100

    adicionei a activity quem vai consigo--- Falta acabar

---
+++ app/src/main/java/com/example/pint_android_v3/quem_vai_consigo.java
@@ -0,0 +1,14 @@
+package com.example.pint_android_v3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quem_vai_consigo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quem_vai_consigo);
+    }
+}
