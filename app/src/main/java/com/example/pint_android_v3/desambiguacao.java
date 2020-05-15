dd238504d6a468355c7c6269b508ffe70fb28afb
NReun <nreunn@gmail.com>
2020-05-15 17:15:33 +0100

    Commit_Menu_ADD

--- app/src/main/java/com/example/pint_android_v3/desambiguacao.java
+++ app/src/main/java/com/example/pint_android_v3/desambiguacao.java
@@ -2,13 +2,31 @@ package com.example.pint_android_v3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class desambiguacao extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("oncreate:", "on create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desambiguacao);
+        TextView Mail = (EditText) findViewById(R.id.user_login_text);
+        TextView Password = (EditText) findViewById(R.id.user_password_text);
+
+        Intent I= getIntent();
+        Bundle b = I.getExtras();
+
+        if(b!=null)
+        {
+            String j =(String) b.get("User_Login");
+            Mail.setText(j);
+            j = (String) b.get("Password_Login");
+            Password.setText(j);
+        }
     }
 }
---
+++ app/src/main/java/com/example/pint_android_v3/menu_municipe.java
@@ -0,0 +1,22 @@
+package com.example.pint_android_v3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menu_municipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("oncreate:", "on create Menu");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desambiguacao);
+
+    }
+
+
+}
