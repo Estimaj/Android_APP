737fdc91270c39184a840b4375ea0e714aa0d52b
NReun <nreunn@gmail.com>
2020-05-16 22:56:14 +0100

    Criação_Array_Adapter_Efetuadas_TESTE

---
+++ app/src/main/java/com/example/pint_android_v3/viagens_test.java
@@ -0,0 +1,40 @@
+package com.example.pint_android_v3;
+
+public class viagens_test {
+
+    private String Local_Partida;
+    private String Local_Chegada;
+    private String data;
+    private String distancia;
+    private String tempo;
+
+    public viagens_test(String Local_Partida, String Local_Chegada, String data, String distancia, String tempo)
+    {
+        this.data = data;
+        this.distancia = distancia;
+        this.Local_Chegada = Local_Chegada;
+        this.Local_Partida = Local_Partida;
+        this.tempo = tempo;
+
+    }
+
+    public String getLocal_Partida() {
+        return Local_Partida;
+    }
+
+    public String getLocal_Chegada() {
+        return Local_Chegada;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getDistancia() {
+        return distancia;
+    }
+
+    public String getTempo() {
+        return tempo;
+    }
+}
