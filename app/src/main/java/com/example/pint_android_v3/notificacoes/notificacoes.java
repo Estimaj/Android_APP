c1a6717b9d213e1702d47cae6da7e85f7b247700
Joao Estima <joao.pedro.estima@hotmail.com>
2020-05-27 13:48:01 +0100

    Adicionei notificacoes (falta o código)

---
+++ app/src/main/java/com/example/pint_android_v3/notificacoes/notificacoes.java
@@ -0,0 +1,16 @@
+package com.example.pint_android_v3.notificacoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pint_android_v3.R;
+
+public class notificacoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificacoes);
+    }
+}
