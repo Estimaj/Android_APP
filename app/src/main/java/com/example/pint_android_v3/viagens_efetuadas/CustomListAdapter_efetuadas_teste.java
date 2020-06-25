a99b8c11c62d079d857a3c0eff2728e8dc5aa25d
NReun <nreunn@gmail.com>
2020-06-25 17:22:55 +0100

    Adapter das viagens marcadas completo falta código

--- app/src/main/java/com/example/pint_android_v3/viagens_efetuadas/CustomListAdapter_efetuadas_teste.java
+++ app/src/main/java/com/example/pint_android_v3/viagens_efetuadas/CustomListAdapter_efetuadas_teste.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.pint_android_v3.R;
@@ -57,12 +56,12 @@ public class CustomListAdapter_efetuadas_teste extends BaseAdapter {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.viagens_efetuadas_adapter, parent, false);
-            viewHolder.txt_data = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_data_trip);
-            viewHolder.txt_tempo = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Tempo);
-            viewHolder.txt_distancia = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Distancia);
-            viewHolder.txt_Local_Chegada = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Local_Chegada);
-            viewHolder.txt_Local_Partida = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Local_Partida);
-            viewHolder.txt_hora = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_hora_trip);
+            viewHolder.txt_data = (TextView)convertView.findViewById(R.id.viagens_marcadas_adapter_data_trip);
+            viewHolder.txt_tempo = (TextView)convertView.findViewById(R.id.viagens_marcadas_adapter_Tempo);
+            viewHolder.txt_distancia = (TextView)convertView.findViewById(R.id.viagens_marcadas_adapter_Distancia);
+            viewHolder.txt_Local_Chegada = (TextView)convertView.findViewById(R.id.viagens_marcadas_adapter_Local_Chegada);
+            viewHolder.txt_Local_Partida = (TextView)convertView.findViewById(R.id.viagens_marcadas_adapter_Local_Partida);
+            viewHolder.txt_hora = (TextView)convertView.findViewById(R.id.viagens_marcadas_adapter_hora_trip);
 
             result = convertView;
             convertView.setTag(viewHolder);
