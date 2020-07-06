e305329d1558ea8ec255a8e0f59fc9b0346487b9
Joao Estima <joao.pedro.estima@hotmail.com>
2020-07-06 15:14:13 +0100

    login update

--- app/src/main/java/com/example/pint_android_v3/DataBase/Post.java
+++ app/src/main/java/com/example/pint_android_v3/DataBase/Post.java
@@ -1,9 +1,14 @@
 package com.example.pint_android_v3.DataBase;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Post {
 
     private int tipo;
-    private int id;
+    @SerializedName("id_user")
+    @Expose
+    private int id; //id_user
     private String message;
 
     public int getTipo() {
