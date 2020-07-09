164c28b28fdbfb2a7d6e333bd2b88652eb71ea35
NReun <nreunn@gmail.com>
2020-07-09 17:40:51 +0100

    Notificaçoes

--- app/src/main/java/com/example/pint_android_v3/DataBase/BaseDadosInterface.java
+++ app/src/main/java/com/example/pint_android_v3/DataBase/BaseDadosInterface.java
@@ -2,6 +2,7 @@ package com.example.pint_android_v3.DataBase;
 
 import com.example.pint_android_v3.DataBase.DadosUtilizador.Model_User_Information;
 import com.example.pint_android_v3.DataBase.DividaUtilizador.ModelDividaUtilizador;
+import com.example.pint_android_v3.DataBase.Notificacoes.ModelNotificacoes;
 import com.example.pint_android_v3.DataBase.ViagensEfetuadas.Model_Viagens_Efetuadas;
 
 import java.util.HashMap;
@@ -28,4 +29,7 @@ public interface BaseDadosInterface {
 
     @GET("/muv/dividas/getcidadaodivida/{id}")
     Call<ModelDividaUtilizador> executeGetUserDivida(@Path("id") int id);
+
+    /*@GET("/muv/notificacoes/getnotificacao/{id}")
+    Call<ModelNotificacoes> executeGetNotificacoes(@Path("id") int id);*/
 }
