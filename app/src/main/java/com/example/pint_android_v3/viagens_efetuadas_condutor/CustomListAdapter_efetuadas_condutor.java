a831ef6888839fc3687ce3035e0d8cbe521e42c2
Joao Estima <joao.pedro.estima@hotmail.com>
2020-07-09 18:29:09 +0100

    acabei agora as viagens efectuadas no motorista

--- app/src/main/java/com/example/pint_android_v3/viagens_efetuadas_condutor/CustomListAdapter_efetuadas_condutor.java
+++ app/src/main/java/com/example/pint_android_v3/viagens_efetuadas_condutor/CustomListAdapter_efetuadas_condutor.java
@@ -1,6 +1,7 @@
 package com.example.pint_android_v3.viagens_efetuadas_condutor;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,32 +9,53 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.pint_android_v3.R;
+import com.example.pint_android_v3.mapas.mais_info_mapa_cliente;
+import com.example.pint_android_v3.mapas.mais_info_mapa_condutor;
+import com.example.pint_android_v3.viagens_efetuadas.CustomListAdapter_efetuadas_teste;
+
+import java.util.ArrayList;
 
 public class CustomListAdapter_efetuadas_condutor extends BaseAdapter {
 
     Context context;
-    private final String[] data;
-    //private final String[] tempo;
-    //private final String[] distancia;
-    private final String[] Local_Chegada;
-    private final String[] Local_Partida;
-    private final String[] hora;
 
-    public CustomListAdapter_efetuadas_condutor(Context cont, String[] data,  String[] local_Chegada, String[] local_Partida, String[] hora) {
+    private final int user_id;
+    private final ArrayList<String> Local_Chegada;
+    private final ArrayList<String> Local_Partida;
+    private final ArrayList<String> Local_PartidaCoordenadas;
+    private final ArrayList<String> Local_ChegadaCoordenadas;
+    private final ArrayList<String> hora;
+    private final ArrayList<String> data;
+    private final ArrayList<Integer> idViagem;
+    private final ArrayList<String> valorViagem;
+    private final ArrayList<Integer> bagagemPedido;
+    private final ArrayList<Integer> animalPedido;
+    private final ArrayList<Integer> necessidadesEspeciaisPedido;
+
+    public CustomListAdapter_efetuadas_condutor(Context cont, int user_id,
+              ArrayList<String> data,  ArrayList<String> local_Chegada,ArrayList<String> Local_ChegadaCoordenadas
+            , ArrayList<String> local_Partida, ArrayList<String> Local_PartidaCoordenadas, ArrayList<String> hora, ArrayList<Integer> idViagem,
+              ArrayList<String> valorViagem, ArrayList<Integer> bagagemPedido,  ArrayList<Integer> animalPedido, ArrayList<Integer> necessidadesEspeciaisPedido) {
 
         context = cont;
-        this.data = data;
-        //this.tempo = tempo;
-        //this.distancia = distancia;
-        Local_Chegada = local_Chegada;
-        Local_Partida = local_Partida;
+        this.user_id = user_id;
+        this.Local_Chegada = local_Chegada;
+        this.Local_Partida = local_Partida;
+        this.Local_ChegadaCoordenadas = Local_ChegadaCoordenadas;
+        this.Local_PartidaCoordenadas = Local_PartidaCoordenadas;
         this.hora = hora;
+        this.data = data;
+        this.idViagem = idViagem;
+        this.valorViagem = valorViagem;
+        this.bagagemPedido = bagagemPedido;
+        this.animalPedido = animalPedido;
+        this.necessidadesEspeciaisPedido = necessidadesEspeciaisPedido;
     }
 
     @Override
     public int getCount() {
         if(Local_Chegada == null) return 0;
-        return Local_Chegada.length;
+        return Local_Chegada.size();
     }
 
     @Override
@@ -49,20 +71,21 @@ public class CustomListAdapter_efetuadas_condutor extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        CustomListAdapter_efetuadas_condutor.ViewHolder viewHolder;
+        ViewHolder viewHolder;
         final View result;
 
         if(convertView == null)
         {
-            viewHolder = new CustomListAdapter_efetuadas_condutor.ViewHolder();
+            viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.viagens_efetuadas_adapter_condutor, parent, false);
-            viewHolder.txt_data = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_data_trip_condutor);
-            //viewHolder.txt_tempo = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Tempo_condutor);
-            //viewHolder.txt_distancia = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Distancia_condutor);
-            viewHolder.txt_Local_Chegada = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Local_Chegada_condutor);
-            viewHolder.txt_Local_Partida = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_Local_Partida_condutor);
-            viewHolder.txt_hora = (TextView)convertView.findViewById(R.id.viagens_efetuadas_adapter_hora_trip_condutor);
+            viewHolder.txt_Local_Chegada = convertView.findViewById(R.id.viagens_efetuadas_adapter_Local_Chegada_condutor);
+            viewHolder.txt_Local_Partida = convertView.findViewById(R.id.viagens_efetuadas_adapter_Local_Partida_condutor);
+            viewHolder.txt_hora = convertView.findViewById(R.id.viagens_efetuadas_adapter_hora_trip_condutor);
+            viewHolder.txt_data = convertView.findViewById(R.id.viagens_efetuadas_adapter_data_trip_condutor);
+            //btn
+            viewHolder.btnMaisInfo = convertView.findViewById(R.id.viagens_efetuadas_adapter_mais_info_text_condutor);
+
 
             result = convertView;
             convertView.setTag(viewHolder);
@@ -73,25 +96,81 @@ public class CustomListAdapter_efetuadas_condutor extends BaseAdapter {
             result = convertView;
         }
 
-        viewHolder.txt_data.setText(data[position]);
-        //viewHolder.txt_distancia.setText(distancia[position]);
-        //viewHolder.txt_tempo.setText(tempo[position]);
-        viewHolder.txt_Local_Chegada.setText(Local_Chegada[position]);
-        viewHolder.txt_Local_Partida.setText(Local_Partida[position]);
-        viewHolder.txt_hora.setText(hora[position]);
+        viewHolder.txt_data.setText(data.get(position));
+        viewHolder.txt_Local_Chegada.setText(Local_Chegada.get(position));
+        viewHolder.txt_Local_Partida.setText(Local_Partida.get(position));
+        viewHolder.txt_hora.setText(hora.get(position));
+        viewHolder.localChegadaCoordenadas = (Local_PartidaCoordenadas.get(position));
+        viewHolder.localPartidaCoordenadas = (Local_ChegadaCoordenadas.get(position));
+        viewHolder.idViagem = idViagem.get(position);
+        viewHolder.valorViagem = valorViagem.get(position);
+        viewHolder.bagagemPedido = bagagemPedido.get(position);
+        viewHolder.animalPedido = animalPedido.get(position);
+        viewHolder.necessidadesEspeciaisPedido = necessidadesEspeciaisPedido.get(position);
+
+        viewHolder.btnMaisInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                maisInfoBtnClick(viewHolder);
+            }
+        });
 
 
-
-        return convertView;
+        return result;
     }
 
     private static class ViewHolder {
         TextView txt_data;
-        //TextView txt_distancia;
-        //TextView txt_tempo;
         TextView txt_Local_Chegada;
         TextView txt_Local_Partida;
         TextView txt_hora;
+        TextView btnMaisInfo;
+        //informação
+        int idViagem;
+        String valorViagem;
+        String localChegadaCoordenadas;
+        String localPartidaCoordenadas;
+        int bagagemPedido;
+        int animalPedido;
+        int necessidadesEspeciaisPedido;
+    }
+
+    public void maisInfoBtnClick(ViewHolder viewHolder)
+    {
+        TextView localPartidatxt = viewHolder.txt_Local_Partida;
+        TextView localChegadatxt = viewHolder.txt_Local_Chegada;
+
+        String localPartidaCoord = viewHolder.localPartidaCoordenadas;
+        String localChegadaCoord = viewHolder.localChegadaCoordenadas;
+
+        TextView dataViagemtxt = viewHolder.txt_data;
+        TextView horaViagemtxt = viewHolder.txt_hora;
+
+        int idViagemCurrent = viewHolder.idViagem;
+        String valorViagemCurrent = viewHolder.valorViagem;
+
+        int bagagemPedidoCurrent = viewHolder.bagagemPedido;
+        int animalPedidoCurrent = viewHolder.animalPedido;
+        int necessidadesEspeciaisPedidoCurrent = viewHolder.necessidadesEspeciaisPedido;
+
+
+
+        //Log.i("text",  localPartidatxt.getText().toString() + "|"+ localChegadatxt.getText().toString());
+        Intent goMaisInfo = new Intent(context, mais_info_mapa_cliente.class);
+        goMaisInfo.putExtra("user_id", user_id);
+        goMaisInfo.putExtra("localPartida", localPartidatxt.getText());
+        goMaisInfo.putExtra("localChegada", localChegadatxt.getText());
+        goMaisInfo.putExtra("localPartidaCoord", localPartidaCoord);
+        goMaisInfo.putExtra("localChegadaCoord", localChegadaCoord);
+        goMaisInfo.putExtra("dataViagem", dataViagemtxt.getText());
+        goMaisInfo.putExtra("horaViagem", horaViagemtxt.getText());
+        goMaisInfo.putExtra("idViagem", idViagemCurrent);
+        goMaisInfo.putExtra("valorViagem", valorViagemCurrent);
+        goMaisInfo.putExtra("bagagemPedido", bagagemPedidoCurrent);
+        goMaisInfo.putExtra("animalPedido", animalPedidoCurrent);
+        goMaisInfo.putExtra("necessidadesEspeciaisPedido", necessidadesEspeciaisPedidoCurrent);
+        goMaisInfo.putExtra("user_id", user_id);
+        context.startActivity(goMaisInfo);
 
     }
 }
--- app/src/main/java/com/example/pint_android_v3/viagens_efetuadas_condutor/viagens_efetuadas_condutor.java
+++ app/src/main/java/com/example/pint_android_v3/viagens_efetuadas_condutor/viagens_efetuadas_condutor.java
@@ -2,10 +2,14 @@ package com.example.pint_android_v3.viagens_efetuadas_condutor;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.example.pint_android_v3.DataBase.BaseDadosInterface;
+import com.example.pint_android_v3.DataBase.ViagensEfetuadas.Model_Viagens_Efetuadas;
+import com.example.pint_android_v3.DataBase.ViagensEfetuadas.dataViagem;
 import com.example.pint_android_v3.R;
 import com.example.pint_android_v3.barra_lateral.barra_lateral_condutor;
 import com.example.pint_android_v3.barra_lateral.barra_lateral_pro;
@@ -16,12 +20,23 @@ import com.example.pint_android_v3.viagens_efetuadas.CustomListAdapter_efetuadas
 import com.example.pint_android_v3.viagens_efetuadas.viagens_efetuadas;
 import com.example.pint_android_v3.viagens_efetuadas.viagens_efetuadas_array_test;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class viagens_efetuadas_condutor extends barra_lateral_condutor {
 
+    private String BASE_URL ="http://10.0.2.2:3000";
     private int user_id;
+    private ArrayList<dataViagem> informacaoViagem;
+
     ListView lView;
     ListAdapter lAdapter;
-    viagens_efetuadas_condutor_array lItems;
+
 
 
     @Override
@@ -29,34 +44,115 @@ public class viagens_efetuadas_condutor extends barra_lateral_condutor {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viagens_efetuadas_condutor);
 
-        lItems = new viagens_efetuadas_condutor_array();
-        lView = (ListView)findViewById(R.id.viagens_efetuadas_listview_condutor);
-        lAdapter = new CustomListAdapter_efetuadas_condutor(viagens_efetuadas_condutor.this,
-                lItems.data,
-                lItems.Local_Chegada, lItems.Local_Partida,lItems.hora);
-
-        lView.setAdapter(lAdapter);
-
         Intent X = getIntent();
         Bundle b = X.getExtras();
         if(b!=null){
             user_id = (int) b.get("user_id");
-            //Log.i("id_user", ""+ id_user);
 
+            getInformationFromdb(user_id);
         }
         Bar_Settings(user_id);
 
 
     }
 
-    public void Mais_Info_Condutor_efetuadas(View view)
-    {
-        Intent GO = new Intent(viagens_efetuadas_condutor.this, mais_info_mapa_condutor.class);
-        GO.putExtra("user_id", user_id);
-        startActivity(GO);
+    private void getInformationFromdb(int id) {
+        if(id == 0) return;
 
+        Retrofit retrofit;
+        BaseDadosInterface baseDadosInterface;
 
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        baseDadosInterface =  retrofit.create(BaseDadosInterface.class);
+
+        //Log.i("O id do user:", ""+ id);
+        Call<Model_Viagens_Efetuadas> call = baseDadosInterface.executeViagemEfetuadaMotorista(id);
+
+        call.enqueue(new Callback<Model_Viagens_Efetuadas>() {
+            @Override
+            public void onResponse(Call<Model_Viagens_Efetuadas> call, Response<Model_Viagens_Efetuadas> response) {
+                if (!response.isSuccessful()){
+                    Log.i("Erro", "verificar o link na interface");
+                }
+                if (response.body().getDataViagem() != null) {
+                    informacaoViagem = response.body().getDataViagem();
+                    createAdapters();
+                }else
+                    Log.i("Erro", "L105 viagens efetuadas");
+            }
+
+            @Override
+            public void onFailure(Call<Model_Viagens_Efetuadas> call, Throwable t) {
+                Log.i("Failure:", t.toString());
+            }
+        });
     }
+
+    private void createAdapters(){
+        ArrayList<String> Local_Partida = new ArrayList<>();
+        ArrayList<String> Local_Chegada = new ArrayList<>();
+        ArrayList<String> Local_PartidaCoordenadas = new ArrayList<>();
+        ArrayList<String> Local_ChegadaCoordenadas = new ArrayList<>();
+        ArrayList<String> data = new ArrayList<>();
+        ArrayList<String> hora = new ArrayList<>();
+        ArrayList<Integer> idViagem = new ArrayList<>();
+        ArrayList<String> valorViagem = new ArrayList<>();
+        ArrayList<Integer> bagagemPedido = new ArrayList<>();
+        ArrayList<Integer> animalPedido = new ArrayList<>();
+        ArrayList<Integer> necessidadesEspeciaisPedido = new ArrayList<>();
+
+        for (int i = 0; i < informacaoViagem.size(); i++) {
+            if(informacaoViagem.get(i).getViagem_efetuada() == 1) { //confirma se a viagem é efetuada ou não
+                Local_Partida.add(informacaoViagem.get(i).getOrigemNome());
+                Local_Chegada.add(informacaoViagem.get(i).getDestinoNome());
+                Local_PartidaCoordenadas.add(informacaoViagem.get(i).getDestino_Coordenadas());
+                Local_ChegadaCoordenadas.add(informacaoViagem.get(i).getOrigem_Coordenadas());
+
+                data.add(informacaoViagem.get(i).getDia_viagem());
+                hora.add(informacaoViagem.get(i).getHora_viagem());
+
+                idViagem.add(informacaoViagem.get(i).getId_viagem());
+                valorViagem.add(informacaoViagem.get(i).getValor_viagem());
+
+                bagagemPedido.add(informacaoViagem.get(i).getBagagem_pedido());
+                animalPedido.add(informacaoViagem.get(i).getAnimal());
+                necessidadesEspeciaisPedido.add(informacaoViagem.get(i).getNecessidadesespeciais_pedido());
+            }
+        }
+        viagens_efetuadas_condutor_array lItems = new viagens_efetuadas_condutor_array(
+                Local_Partida, Local_Chegada,
+                Local_PartidaCoordenadas, Local_ChegadaCoordenadas,
+                data, hora, idViagem,
+                valorViagem, bagagemPedido,
+                animalPedido, necessidadesEspeciaisPedido
+        );
+
+
+        lView = findViewById(R.id.viagens_efetuadas_listview_condutor);
+        lAdapter = new CustomListAdapter_efetuadas_condutor(
+                viagens_efetuadas_condutor.this,
+                user_id,
+                lItems.data,
+                lItems.Local_Chegada,
+                lItems.Local_ChegadaCoordenadas,
+                lItems.Local_Partida,
+                lItems.Local_PartidaCoordenadas,
+                lItems.hora,
+                lItems.idViagem,
+                lItems.valorViagem,
+                lItems.bagagemPedido,
+                lItems.animalPedido,
+                lItems.necessidadesEspeciaisPedido);
+
+
+
+        lView.setAdapter(lAdapter);
+    }
+
+
 
 
 }
