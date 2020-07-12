25fd73fecdb2d221fc6acbf09afbdcb65d2b80d5
NReun <nreunn@gmail.com>
2020-07-12 16:14:14 +0100

    Notificacoes Condutor done

--- app/src/main/java/com/example/pint_android_v3/notificacoes/notificacoes_condutor.java
+++ app/src/main/java/com/example/pint_android_v3/notificacoes/notificacoes_condutor.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.pint_android_v3.DataBase.BaseDadosInterface;
+import com.example.pint_android_v3.DataBase.Notificacoes.DataNotificacoes;
+import com.example.pint_android_v3.DataBase.Notificacoes.ModelNotificacoes;
 import com.example.pint_android_v3.R;
 import com.example.pint_android_v3.barra_lateral.barra_lateral_condutor;
 import com.example.pint_android_v3.barra_lateral.barra_lateral_pro;
@@ -16,12 +21,23 @@ import com.example.pint_android_v3.viagens_efetuadas.CustomListAdapter_efetuadas
 import com.example.pint_android_v3.viagens_efetuadas.viagens_efetuadas;
 import com.example.pint_android_v3.viagens_efetuadas.viagens_efetuadas_array_test;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class notificacoes_condutor extends barra_lateral_condutor {
 
     private int user_id;
     ListView lView;
     ListAdapter lAdapter;
     notificacoes_array_test lItems;
+    private String BASE_URL ="http://10.0.2.2:3000";
+    private ArrayList<DataNotificacoes> informacaoViagem;
+    private TextView Nome;
 
 
     @Override
@@ -38,16 +54,93 @@ public class notificacoes_condutor extends barra_lateral_condutor {
             //Log.i("id_user", ""+ id_user);
 
         }
+        getInformationFromdb(user_id);
         Bar_Settings(user_id);
 
 
     }
 
-    public void MaisInfo(View view){
-        Intent MaisInfo = new Intent(notificacoes_condutor.this, notificacoes_mais_info_condutor.class);
-        MaisInfo.putExtra("user_id", user_id);
-        startActivity(MaisInfo);
+
+    private void getInformationFromdb(int id) {
+        if(id == 0) return;
+
+        Retrofit retrofit;
+        BaseDadosInterface baseDadosInterface;
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        baseDadosInterface =  retrofit.create(BaseDadosInterface.class);
+
+        //Log.i("O id do user:", ""+ id);
+        Call<ModelNotificacoes> call = baseDadosInterface.executeGetNotificacoes(id);
+
+        call.enqueue(new Callback<ModelNotificacoes>() {
+            @Override
+            public void onResponse(Call<ModelNotificacoes> call, Response<ModelNotificacoes> response) {
+                if (!response.isSuccessful()){
+                    Log.i("Erro", "L99 viagens efetuadas");
+                }
+                if (response.code() == 200){
+                    if (response.body().getDataViagem() != null) {
+                        //Log.i("body", "" + response.body().getDataViagem().get(0).toString());
+                        informacaoViagem = response.body().getDataViagem();
+                        createAdapters();
+                    }else
+                        Log.i("Erro", "L105 viagens efetuadas");
+                }
+                else{
+                    Log.i("Erro", "L109 viagens efetuadas_sem data");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ModelNotificacoes> call, Throwable t) {
+                Log.i("Failure:", t.toString());
+                //makeToastFordesambiguacao("Failure: "+ t.toString());
+            }
+        });
+    }
+
+    public void createAdapters(){
+        ArrayList<String> ListNome = new ArrayList<>();
+        ArrayList<String> ListData = new ArrayList<>();
+        ArrayList<Integer> ListTipo = new ArrayList<>();
+        ArrayList<Integer> ListIDViagem = new ArrayList<>();
+
+
+
+        for (int i = 0; i < informacaoViagem.size(); i++) {
+            ListNome.add(informacaoViagem.get(i).getNome());
+            ListData.add(informacaoViagem.get(i).getDia_viagem());
+            ListTipo.add(informacaoViagem.get(i).getTipo_notif());
+            ListIDViagem.add(informacaoViagem.get(i).getId_viagem());
+
+        }
+
+        notificacoes_array_test lItems = new notificacoes_array_test(ListNome, ListData, ListTipo, ListIDViagem);
+
+
+        lView = findViewById(R.id.listView_para_layout_notificacoes);
+        lAdapter = new Custom_Adapter_Notificacoes(
+                notificacoes_condutor.this,
+                user_id,
+                ListData,
+                ListNome,
+                ListTipo,
+                ListIDViagem,
+                1
+        );
+
+
+
+        lView.setAdapter(lAdapter);
     }
 
 
+
+
 }
+
+
